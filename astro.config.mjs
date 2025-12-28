beae22fab442b5c9a773dfe6f37a5aff41de3b84
AnthoniDev95 <castilloantony676@gmail.com>
2025-12-28 17:20:22 -0400

    VercelConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-
+    output: 'static',
+    adapter: vercel({
+        
+    }),
 });
