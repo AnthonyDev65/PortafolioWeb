00fce31a947240a0f8e4ae57d86e242476026837
AnthoniDev95 <castilloantony676@gmail.com>
2026-01-06 11:45:37 -0400

    Pre v2.0.1

---
+++ src/scripts/ExperienceBase.js
@@ -0,0 +1,15 @@
+const Experience = [
+    {
+        App : "Fonicris",
+        Descripcion : "App para gestionar Archivos",
+        Categoria : "Experiencias de Trabajo",
+        tecnologias : [
+            "C#", "XAML", ".Net"
+        ],
+        BadgeText : 'Reciente',
+        BadgeColor : ' ',
+        link : ' ',
+    },
+
+
+]
