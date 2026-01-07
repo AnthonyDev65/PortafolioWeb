86fda7b1442e241c535f452643a9b3ce960b72ab
AnthoniDev95 <castilloantony676@gmail.com>
2026-01-06 22:10:59 -0400

    Beta 2.0

--- src/scripts/ExperienceBase.js
+++ src/scripts/ExperienceBase.js
@@ -1,20 +1,8 @@
 export const Experience = [
     {
         App : "Fonicris",
-        Descripcion : "App para gestionar Archivos",
-        Categoria : "Experiencias de Trabajo",
-        tecnologias : [
-            "C#", "XAML", ".Net"
-        ],
-        badge : true,
-        BadgeText : 'Reciente',
-        BadgeColor : ' ',
-        link : ' ',
-    },
-    {
-        App : "Technician",
-        Descripcion : "App para gestionar clientes de redes",
-        Categoria : "Experiencias de Trabajo",
+        Descripcion : "App to manage assets",
+        Categoria : "Work Experiences",
         tecnologias : [
             "C#", "XAML", ".Net"
         ],
@@ -22,7 +10,53 @@ export const Experience = [
         BadgeText : 'Reciente',
         BadgeColor : ' ',
         link : ' ',
+        img : '../src/components/Version 2/App/Fonicris.jpg',
+        statusColor : 'bg-white',
+        statusIcon: 'black',
     },
-
-
+    {
+        App : "Technician",
+        Descripcion : "App to manage clients",
+        Categoria : "Work Experiences",
+        tecnologias : [
+            "C#", "XAML", ".Net"
+        ],
+        badge : false,
+        BadgeText : 'Reciente',
+        BadgeColor : ' ',
+        link : ' ',
+        img : '../src/components/Version 2/App/Technician.jpg',
+        statusColor : 'bg-[#f4f4f4]',
+        statusIcon: 'black',
+    },
+    {
+        App : "Unitasks",
+        Descripcion : "University app for managing classes and subjects",
+        Categoria : "Personal Experiences",
+        tecnologias : [
+            "C#", "XAML", ".Net"
+        ],
+        badge : false,
+        BadgeText : 'Reciente',
+        BadgeColor : ' ',
+        link : ' ',
+        img : '../src/components/Version 2/App/Unitasks.jpg',
+        statusColor : 'bg-white',
+        statusIcon: 'black',
+    },
+    {
+        App : "PerfectSounds",
+        Descripcion : "Music app for scales and notes",
+        Categoria : "Personal Experiences",
+        tecnologias : [
+            "C#", "XAML", ".Net"
+        ],
+        badge : false,
+        BadgeText : 'Reciente',
+        BadgeColor : ' ',
+        link : ' ',
+        img : '../src/components/Version 2/App/PerfectSounds.jpg',
+        statusColor : 'bg-[#2E2E36]',
+        statusIcon: 'white',
+    },
 ]
