bc19750131a0c6748229c67d65244e4cd666910e
AnthoniDev95 <castilloantony676@gmail.com>
2026-01-06 20:40:30 -0400

    Pre v2.0.3

--- src/scripts/ExperienceBase.js
+++ src/scripts/ExperienceBase.js
@@ -6,6 +6,19 @@ export const Experience = [
         tecnologias : [
             "C#", "XAML", ".Net"
         ],
+        badge : true,
+        BadgeText : 'Reciente',
+        BadgeColor : ' ',
+        link : ' ',
+    },
+    {
+        App : "Technician",
+        Descripcion : "App para gestionar clientes de redes",
+        Categoria : "Experiencias de Trabajo",
+        tecnologias : [
+            "C#", "XAML", ".Net"
+        ],
+        badge : false,
         BadgeText : 'Reciente',
         BadgeColor : ' ',
         link : ' ',
